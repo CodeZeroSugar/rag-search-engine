de4a2baba06599e6980c299dd2804fdb7a04d479
Ian Velder <icvelder@gmail.com>
2026-02-26 23:34:07 -0600

    implemented tokenized matching

--- cli/utils.py
+++ cli/utils.py
@@ -1,4 +1,5 @@
 import json
+import string
 from pathlib import Path
 
 
@@ -8,13 +9,39 @@ def get_movies_path():
     return data_path
 
 
+def clean_input(input):
+    lower = input.lower()
+    trans_table = str.maketrans("", "", string.punctuation)
+    return lower.translate(trans_table)
+
+
+def tokenize(input):
+    final_tokens = []
+    tokens = input.split()
+    for token in tokens:
+        if token == "":
+            continue
+        final_tokens.append(token)
+    return final_tokens
+
+
 def search_movies(query):
     movie_results = []
     with open(get_movies_path(), "r") as f:
         data = json.load(f)
+    query_tokens = tokenize(clean_input(query))
     for movie in data["movies"]:
         if len(movie_results) == 5:
             break
-        if query.lower() in movie["title"].lower():
-            movie_results.append(movie["title"])
+        found = False
+        movie_tokens = tokenize(clean_input(movie["title"]))
+        for query_token in query_tokens:
+            for movie_token in movie_tokens:
+                if query_token in movie_token:
+                    movie_results.append(movie["title"])
+                    found = True
+                    break
+            if found:
+                break
+
     return movie_results
