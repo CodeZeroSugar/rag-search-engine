942a20684e1a591fd43198d71cac6f940916ac14
Ian Velder <icvelder@gmail.com>
2026-02-27 22:53:08 -0600

    implementing frequency

--- cli/indexing.py
+++ cli/indexing.py
@@ -1,5 +1,5 @@
+from collections import Counter
 import json
-import os
 from pickle import dump, load
 from pathlib import Path
 from utils import get_data_file, tokenize
@@ -9,13 +9,17 @@ class InvertedIndex:
     def __init__(self):
         self.index = dict()
         self.docmap = dict()
+        self.term_frequencies = dict()
 
     def __add_document(self, doc_id, text):
         tokens = tokenize(text)
+        if doc_id not in self.term_frequencies:
+            self.term_frequencies[doc_id] = Counter()
         for token in tokens:
             if token not in self.index:
                 self.index[token] = set()
             self.index[token].add(doc_id)
+            self.term_frequencies[doc_id][token] += 1
 
     def get_documents(self, term):
         term_lower = term.lower()
@@ -41,16 +45,29 @@ class InvertedIndex:
             dump(self.index, index)
         with open(cache_path / "docmap.pkl", "wb") as docmap:
             dump(self.docmap, docmap)
+        with open(cache_path / "term_frequencies.pkl", "wb") as term_freq:
+            dump(self.term_frequencies, term_freq)
 
     def load(self):
         base_dir = Path(__file__).resolve().parent
         cache_path = base_dir.parent / "cache"
         index_path = cache_path / "index.pkl"
         map_path = cache_path / "docmap.pkl"
-        if not index_path.exists() or not map_path.exists():
+        freq_path = cache_path / "term_frequencies.pkl"
+        if not index_path.exists() or not map_path.exists() or not freq_path.exists():
             raise FileNotFoundError("Cache file not found")
         with open(index_path, "rb") as f:
-            index = load(f)
+            self.index = load(f)
         with open(map_path, "rb") as f:
-            docmap = load(f)
-        return index, docmap
+            self.docmap = load(f)
+        with open(freq_path, "rb") as f:
+            self.term_frequencies = load(f)
+
+    def get_tf(self, doc_id, term):
+        tokens = tokenize(term)
+        if len(tokens) != 1:
+            raise Exception("get_tf only accepts 1 term")
+        token = tokens[0]
+        if self.term_frequencies[doc_id].get(token) is None:
+            return 0
+        return self.term_frequencies[doc_id][token]
